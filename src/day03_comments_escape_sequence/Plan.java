954a52a8db349f077237c987014b7643550e3a32
Elgun <elgun>
2023-09-10 17:27:57 -0400

    Java Update

--- src/day03_comments_escape_sequence/Plan.java
+++ src/day03_comments_escape_sequence/Plan.java
@@ -1,7 +1,6 @@
 package day03_comments_escape_sequence;
 
 public class Plan {
-
     public static void main(String[] args) {
         System.out.println("Learning Java");
         System.out.println("Get into Group Studies");
