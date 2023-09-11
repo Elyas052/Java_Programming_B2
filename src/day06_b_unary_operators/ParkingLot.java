1cb4ae19960932708587a3c9407985adafb0b573
Elgun <elgun>
2023-09-10 23:14:59 -0400

    Java Update 2

--- src/day06_b_unary_operators/ParkingLot.java
+++ src/day06_b_unary_operators/ParkingLot.java
@@ -3,20 +3,20 @@ package day06_b_unary_operators;
 public class ParkingLot {
     public static void main(String[] args) {
         int cars = 10;
-        System.out.println("2 cars drive into the parking lot");
+        System.out.println("\n2 cars drive into the parking lot");
         // approach 1:
 
         cars = cars + 2;
         System.out.println(cars);
 
-        System.out.println("2 more cars drive in");
+        System.out.println("\n2 more cars drive in");
 
         // approach 2:
         cars++;
         cars++;
         System.out.println(cars);
 
-        System.out.println("5 cars left");
+        System.out.println("\n5 cars left");
 
         cars = cars - 5;
         System.out.println(cars);
--- src/day06_b_unary_operators/Shopoing.java
+++ src/day06_b_unary_operators/Shopoing.java
@@ -5,11 +5,11 @@ public class Shopoing {
         int numberOfItems = 0;
         double price = 0.0;
 
-        System.out.println("we pick up 1 water");
-        System.out.println("how many items in the cart: " + ++numberOfItems); // preincrement, so the number of items increases right away, then it is printed
+        System.out.println("\nWe pick up 1 water");
+        System.out.println("how many items in the cart: " + ++numberOfItems); // pre increment, so the number of items increases right away, then it is printed
         price = price + 2.5; // take the price value, which 0, and add 2.5, and they reassign to the price variable
 
-        System.out.println("we pick up 2 eggs");
+        System.out.println("\nWe pick up 2 eggs");
         System.out.println("how many items in the cart: " + ++numberOfItems );
         System.out.println("how many items in the cart: " + ++numberOfItems);
 
