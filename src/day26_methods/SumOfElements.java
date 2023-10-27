aaba8ed95e8c97ea1b87e0b8383ee61e2b024de3
Elyas052 <elyasshiraliyev@gmail.com>
2023-10-26 22:21:52 -0400

    Methods

---
+++ src/day26_methods/SumOfElements.java
@@ -0,0 +1,28 @@
+package day26_methods;
+
+import my_utilities.ArrayUtil;
+
+public class SumOfElements {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 5, 6, 2};
+        // Since I am using the method from the same class, I can call it with just the method name.
+        System.out.println(sumOfArray(arr));
+
+        // Since I am using the method from different package,
+        // I need to import the package first, then use the class name to call method which is inside the class
+        System.out.println(ArrayUtil.sumOfArray(arr));
+
+        System.out.println(sumOfArray(4, 5, 6, 7));
+    }
+
+    // This parameter is called VAR ARGS -- > which is the same data type of elements - > same as an array.
+    public static int sumOfArray(int... arr) {
+        int sum = 0;
+
+        for (int eachNum : arr) {
+            sum += eachNum;
+        }
+        return sum;
+    }
+}
