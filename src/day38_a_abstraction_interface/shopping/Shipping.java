02471c672c4393135a513b44082d91deca88f8de
Elyas052 <elyasshiraliyev@gmail.com>
2023-11-22 22:17:24 -0500

    interface, abstraction recap

---
+++ src/day38_a_abstraction_interface/shopping/Shipping.java
@@ -0,0 +1,11 @@
+package day38_a_abstraction_interface.shopping;
+
+// Interface - we cannot instantiate / cannot create an object
+// all variables are - > public static final
+// the methods by default is - > public abstract
+public interface Shipping {
+
+    String COUNTRY = "US"; // public static final
+
+    void payForShipping(boolean b); // public abstract
+}
