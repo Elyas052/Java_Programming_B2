589b0d31ff82101229d55116691d681c511d471e
Elyas052 <elyasshiraliyev@gmail.com>
2023-12-07 01:35:04 -0500

    recap

--- src/day43_map/enum_intro/Browser.java
+++ src/day43_map/enum_intro/Browser.java
@@ -1,9 +1,9 @@
 package day43_map.enum_intro;
 
 // enum --- > Enumeration
-// It is a special datatype that is MOSTLY used to define the CONSTANTS
+// It is a special datatype that is MOSTLY used to define the CONSTANTS.
 // Usually written in UPPERCASE
-// They act like class.So, they can have variable (MOSTLY Used for variables) and methods
+// They act like class.So, they can have variable (MOSTLY Used for variables) and methods.
 
 public enum Browser {
 
